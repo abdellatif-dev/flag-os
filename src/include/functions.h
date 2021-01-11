62b152bd30458f9761609a023978673b3d92e13d
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-01-11 10:36:16 +0000

    printchar

--- src/include/functions.h
+++ src/include/functions.h
@@ -3,4 +3,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+void printchar(char ch, uint8 color){
+  string vidmem = (string)0xb8000;
+  vidmem[0] = ch;
+  vidmem[1] = color;
+  
+}
+
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,5 @@
 #include "include/functions.h"
 
 flagmain () {
-
+  printchar('A', 0x04);
 }
